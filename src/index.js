6c5653a6d630e85baf4da73c3f04fe081a17ea2e
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-12-20 17:01:45 +0530

    setup nodemailer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { PORT } from './config/serverConfig.js'
 import connectDB from './config/dbConfig.js';
 import apiRouter from './routes/apiRouter.js'
 import pingRoute from './routes/pingRoute.js'
+import nodemailer from './config/mailConfig.js'
 
 const app = express()
 
@@ -12,7 +13,13 @@ app.use(express.urlencoded({extended:true}));
 app.use('/api',apiRouter);
 app.use('/ping',pingRoute)
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
   connectDB();
   console.log(`app running on port: ${PORT}`);
+  // const mailResponse = nodemailer.sendMail({
+  //   from:'shaikhmuzffar82@gmail.com',
+  //   to:'shaikhmuzffar82@gmail.com'
+  // });
+
+  // console.log(mailResponse);
 })
