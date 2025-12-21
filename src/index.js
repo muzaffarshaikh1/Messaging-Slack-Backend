2849b2755fb09b8f5b02ed94f9b3d15899b65d43
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-12-21 23:01:46 +0530

    added socket.io setup

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import { PORT } from './config/serverConfig.js'
 import connectDB from './config/dbConfig.js';
 import apiRouter from './routes/apiRouter.js'
 import pingRoute from './routes/pingRoute.js'
-
 import bullServerAdapator from './config/bullBoardConfig.js';
+
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 const app = express()
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -14,7 +18,14 @@ app.use('/ui',bullServerAdapator.getRouter());
 app.use('/api',apiRouter);
 app.use('/ping',pingRoute)
 
-app.listen(PORT, async() => {
+io.on('connection', (socket) => {
+  console.log('a user connected',socket.id);
+  socket.on('messageFromClient',(data)=>{
+    io.emit('serverMessageBrodcast',data.toUpperCase());
+  })
+});
+
+server.listen(PORT, async() => {
   connectDB();
   console.log(`app running on port: ${PORT}`);
 })
