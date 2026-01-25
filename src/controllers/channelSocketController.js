b29ba1f5330a844915a35fd929b6e68ddd967a58
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2026-01-25 21:19:21 +0530

    handled errors in sockets

--- src/controllers/channelSocketController.js
+++ src/controllers/channelSocketController.js
@@ -1,11 +1,14 @@
 import { JOIN_CHANNEL } from "../utils/common/eventConstants.js";
 
 export default function messageHandlers(io,socket){
+    console.log("messageHandlers called")
     socket.on(JOIN_CHANNEL,async function joinChannelHandler(data,cb){
         const roomId = data.channelId;
+        
         console.log(`user joined room: ${roomId}`);
         socket.join(roomId);
-        cb({
+        console.log(`User ${socket.id} joined the channel: ${roomId}`);
+        cb?.({
             success:true,
             message:"Successfully joined the channel",
             data:roomId
